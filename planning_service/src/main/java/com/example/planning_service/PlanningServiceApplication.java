9d56e36f0e625579e428eca81f8736dc9e33f095
saadalaoui23 <saadalaouisosse@gmail.com>
2025-12-12 19:31:26 +0100

    finalisation des services

--- planning_service/src/main/java/com/example/planning_service/PlanningServiceApplication.java
+++ planning_service/src/main/java/com/example/planning_service/PlanningServiceApplication.java
@@ -2,12 +2,12 @@ package com.example.planning_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients; 
 
 @SpringBootApplication
+@EnableFeignClients
 public class PlanningServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PlanningServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PlanningServiceApplication.class, args);
+    }
 }

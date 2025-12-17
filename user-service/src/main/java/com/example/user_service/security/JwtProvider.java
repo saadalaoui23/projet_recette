8d50de705aa5b55a54d90df26198d55adce5d246
Saad Alaoui <saadalaouisosse@gmail.com>
2025-12-17 21:44:32 +0100

    login backend fixed

--- user-service/src/main/java/com/example/user_service/security/JwtProvider.java
+++ user-service/src/main/java/com/example/user_service/security/JwtProvider.java
@@ -42,8 +42,8 @@ public class JwtProvider {
     // 2. RECUPERATION DE LA CLE SECRETE
     // ----------------------------------------------------
     private Key getSigningKey() {
-        // La clé doit être au format base64
-        byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
+        // Utilisez getBytes() au lieu du décodeur Base64 pour éviter les erreurs de format
+        byte[] keyBytes = jwtSecret.getBytes(java.nio.charset.StandardCharsets.UTF_8);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
